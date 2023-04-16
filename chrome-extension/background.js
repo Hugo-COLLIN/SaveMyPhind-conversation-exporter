5e19f739c21cc0f9d8d0b97896de008e7bf470b6
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-04-16 09:39:07 +0200

    cleaning code

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,18 +1,13 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  console.log("Action loaded.");
   if (changeInfo.status === 'complete' && /^http/.test(tab.url))
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       files: ['turndown.js']
     })
-      .then(() => {
-        console.log("Local Library ok.");
-      })
       .catch(err => console.log(err));
 });
 
 chrome.action.onClicked.addListener(async (tab) => {
-  console.log("Action clicked.");
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
     files: ['main.js']
