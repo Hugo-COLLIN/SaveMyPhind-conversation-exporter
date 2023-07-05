2c98d2d3d5ace18c804e57ba21a8c6f34359f020
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-07-05 13:37:37 +0200

    library licenses list

--- manage/updateLicenseList.js
+++ manage/updateLicenseList.js
@@ -1,1 +1,1 @@
-require('fs').writeFileSync('licenseList.md', require('fs').readFileSync('licenseList.md', 'utf8').split('\n').slice(2).join('\n'))
+require('fs').writeFileSync('licenseList.md', "# Libraries licenses\n" + require('fs').readFileSync('licenseList.md', 'utf8').split('\n').slice(3).join('\n'))
