bada9bfb0ed9e9f9eedabbe8ca8077284af733d6
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-07-05 12:43:38 +0200

    Manifest v3 convert script, without using convert-manifest-format library

---
+++ manage/convertManifest.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+// Get the target browser from the command line arguments
+const targetBrowser = process.argv[2];
+
+// Read the manifest from a file
+const manifest = JSON.parse(fs.readFileSync('./src/manifest.json', 'utf8'));
+
+function convertManifest(manifest) {
+  const convertedManifest = { ...manifest };
+
+  switch(targetBrowser) {
+    case 'chrome':
+      if(manifest.background && manifest.background.tasks) {
+        convertedManifest.background = {
+          service_worker: manifest.background.tasks[0]
+        };
+      }
+      break;
+    case 'firefox':
+      if(manifest.background && manifest.background.service_worker) {
+        convertedManifest.background = {
+          tasks: [manifest.background.service_worker]
+        };
+      }
+      break;
+    default:
+      throw new Error('Browser not supported');
+  }
+
+  return convertedManifest;
+}
+
+// Use the function to convert the manifest
+const convertedManifest = convertManifest(manifest);
+
+// Write the converted manifest
+fs.writeFileSync('./src/manifest.json', JSON.stringify(convertedManifest, null, 2));
+
+console.log('Manifest converted and saved successfully.');
