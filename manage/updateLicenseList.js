f5e6699e9c48000a96b7306e644c65abd82d696a
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-07-18 19:02:59 +0200

    license list update in package.json

--- manage/updateLicenseList.js
+++ manage/updateLicenseList.js
@@ -2,22 +2,33 @@
 
 const fs = require('fs');
 
-console.log(process.argv)
-let licenses = JSON.parse(process.argv[2]);
-console.log(licenses)
+const { exec } = require('child_process');
 
-let formatted = '';
-for (let dep in licenses) {
-  formatted += `${dep}\n`;
-  let props = Object.keys(licenses[dep]);
-  for (let i = 0; i < props.length; i++) {
-    let prop = props[i];
-    if (prop !== 'path' && prop !== 'licenseFile') {
+exec('npx license-checker --production --json', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+
+  const licenses = JSON.parse(stdout);
+
+  console.log(licenses)
+
+  let formatted = '';
+  for (let dep in licenses) {
+    formatted += `${dep}\n`;
+
+    // Filter out 'path' and 'licenseFile' before the loop
+    let props = Object.keys(licenses[dep]).filter(prop => prop !== 'path' && prop !== 'licenseFile');
+
+    for (let i = 0; i < props.length; i++) {
+      let prop = props[i];
       let prefix = (i === props.length - 1) ? '└─' : '├─';
       formatted += `${prefix} ${prop}: ${licenses[dep][prop]}\n`;
     }
+    formatted += '\n';
   }
-  formatted += '\n';
-}
 
-fs.writeFileSync('licenses.txt', formatted);
+
+  fs.writeFileSync('licenses.txt', formatted);
+});
