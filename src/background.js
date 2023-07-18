8259b3f1c3354c97b7c9ce268ff9cb919cf194a6
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-07-18 22:21:07 +0200

    extension icon updated according to website : new page + when extension installed

--- src/background.js
+++ src/background.js
@@ -4,3 +4,30 @@ chrome.action.onClicked.addListener(async (tab) => {
     files: ['main.js']
   });
 });
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  setIcon(changeInfo, tabId);
+});
+
+chrome.runtime.onInstalled.addListener(async function () {
+  const tabsWindow = await chrome.tabs.query({currentWindow: false});
+  console.log(tabsWindow)
+  for (const tab of tabsWindow) {
+    console.log(tab)
+    console.log(tab.url)
+    setIcon({url: tab.url}, tab.id)
+  }
+}
+);
+
+function setIcon(changeInfo, tabId) {
+  if (changeInfo.url) {
+    if (changeInfo.url.includes("phind.com")) {
+      chrome.action.setIcon({ path: { "32": "img/download_icons/icon_phind-32.png"}, tabId: tabId });
+      console.log("passed1")
+    } else {
+      chrome.action.setIcon({ path: { "32": "img/download_icons/icon_web-32.png"}, tabId: tabId });
+      console.log("passed2")
+    }
+  }
+}
