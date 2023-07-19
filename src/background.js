1d8c8d48e425e031105f623fc33eac97af3979da
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-07-19 11:29:21 +0200

    reduce authorization to activeTab + web icon is default

--- src/background.js
+++ src/background.js
@@ -13,28 +13,36 @@ chrome.action.onClicked.addListener(async (tab) => {
 --- This is the code for the extension icon to change depending on the website ---
  */
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  // Enables the icon to be set when the tab is reloaded
-  chrome.tabs.get(tabId, function(tab) {
-    setIcon({url: tab.url}, tabId);
-  });
+  setIconForCurrentTab(tabId);
 });
 
 chrome.runtime.onInstalled.addListener(async function () {
-  const tabsWindow = await chrome.tabs.query({currentWindow: false});
-  for (const tab of tabsWindow) {
-    setIcon({url: tab.url}, tab.id)
-  }
-}
-);
+  setIconForCurrentTab((await chrome.tabs.query({ active: true, currentWindow: true }))[0].id);
+});
 
-function setIcon(changeInfo, tabId) {
-  if (changeInfo.url) {
-    if (changeInfo.url.includes("phind.com")) {
-      chrome.action.setIcon({ path: { "48": "img/icons/icon_phind-48.png"}, tabId: tabId });
-    } else if (changeInfo.url.startsWith('http://') || changeInfo.url.startsWith('https://')) {
-      chrome.action.setIcon({ path: { "48": "img/icons/icon_web-48.png"}, tabId: tabId });
-    } else {
-      chrome.action.setIcon({ path: { "48": "img/icons/icon_disabled-48.png"}, tabId: tabId });
-    }
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  setIconForCurrentTab(activeInfo.tabId);
+});
+
+
+/**
+ * Enables the icon to be set when the tab is reloaded
+ * @param tabId the id of the tab that is reloaded
+ */
+function setIconForCurrentTab(tabId)
+{
+  chrome.tabs.get(tabId, (tab) => {
+    setIcon(tab.url, tabId);
+  });
+}
+
+/**
+ * Sets the icon depending on the url of the tab
+ * @param url the url of the tab
+ * @param tabId the id of the tab
+ */
+function setIcon(url, tabId) {
+  if (url.includes("phind.com")) {
+    chrome.action.setIcon({ path: { "48": "img/icons/icon_phind-48.png"}, tabId: tabId });
   }
 }
