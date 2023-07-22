3146e007d8423ec6219e5909435a9482323f1d28
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-07-22 10:36:21 +0200

    need to integrate and test the solution

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,32 +1,53 @@
 import {catchContent} from "./activeTab/extractor/extractor";
 
-// Dans le contentScript
 document.addEventListener('DOMContentLoaded', function() {
-  // Créer un nouveau bouton
   let button = document.createElement('button');
   button.innerHTML = 'Send Message';
 
-  // Ajouter un écouteur d'événements au bouton
   button.addEventListener('click', function() {
-    // Envoyer le message quand le bouton est cliqué
     chrome.runtime.sendMessage({message: 'exportAllThreads', length: document.querySelectorAll(".table-responsive tr").length}, function(response) {
       console.log(response);
     });
   });
 
-  // Ajouter le bouton au corps du document
   document.body.appendChild(button);
 });
 
-
 chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
   if (request.message === 'clickOnThread') {
-    await document.querySelectorAll(".table-responsive tr")[request.index].click();
-    return true;  // will respond asynchronously
+    document.querySelectorAll(".table-responsive tr")[request.index].click();
   }
 
   if (request.message === 'exportCurrentThread') {
     // export method
-    return true;  // will respond asynchronously
+    console.log("exportCurrentThread");
   }
+  return true;  // will respond asynchronously
 });
+
+// vvvvvvvvvvvvv TODO: test
+//document.addEventListener('DOMContentLoaded', function() {
+//   chrome.runtime.sendMessage({message: 'READY'}, function(response) {
+//     console.log(response);
+//   });
+//
+//   chrome.runtime.sendMessage({message: 'exportAllThreads', length: document.querySelectorAll(".table-responsive tr").length}, function(response) {
+//     console.log(response);
+//   });
+// });
+//
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.message === 'clickOnThread') {
+//     document.querySelectorAll(".table-responsive tr")[request.index].click();
+//     return true;  // will respond asynchronously
+//   }
+//   if (request.message === 'exportCurrentThread') {
+//     // Add your code here to export the current thread
+//     return true;  // will respond asynchronously
+//   }
+//   if (request.message === 'checkIfContentExists') {
+//     // Add your code here to check if the content exists and return true if so
+//     return true;  // will respond asynchronously
+//   }
+//   return true;  // will respond asynchronously
+// });
