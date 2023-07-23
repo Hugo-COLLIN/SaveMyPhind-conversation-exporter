efe9b33c784833bf3be3b5e10eac84d6118229b7
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-07-23 11:58:05 +0200

    main executed when import module main ; now export current page, click on 1st element and stops

--- src/threadFromList.js
+++ src/threadFromList.js
@@ -1,9 +1,19 @@
-import {main} from "./main";
+import {welcome} from "./activeTab/welcome/welcome";
+import {catchContent} from "./activeTab/extractor/extractor";
+import {exporter} from "./activeTab/exporter/exporter";
 
-export function threadFromList(index) {
-  main().then(r =>{
-    console.log("main done");
-    document.querySelectorAll('.table-responsive tr')[index].click();
-  });
+export async function threadFromList(index) {
+  if (document.URL.includes("phind.com")) {
+    await welcome();
+    const caught = await catchContent();
+    if (caught !== null) {
+      await exporter(caught.markdownContent, caught.title);
+      console.log("Export done!")
+    }
+  }
+  // main().then(r =>{
+  console.log("main done");
+  document.querySelectorAll('.table-responsive tr')[index].click();
+  // });
 
 }
