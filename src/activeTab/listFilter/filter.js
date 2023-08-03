a5cba2409ff9b558d6cefbda6ce40ccccff5bbbe
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-08-03 23:57:41 +0200

    filterList

--- src/activeTab/listFilter/filter.js
+++ src/activeTab/listFilter/filter.js
@@ -6,19 +6,19 @@ export async function addListFilter() {
   const input = await createSmallField('Search previous threads...');
   await waitAppend('.container.p-0.mt-6 > .row', [input], 'insertBefore')
 
-  // Filter the list based on the user input
-  function filterList(rowsSelector, textSelector) {
-    const filterText = input.value.toLowerCase();
-    const rows = document.querySelectorAll(rowsSelector);
-
-    rows.forEach(row => {
-      const text = row.querySelector(textSelector).textContent.toLowerCase();
-      row.style.display = text.includes(filterText) ? '' : 'none';
-    });
-  }
-
   // Event listener for changes in the text field
   input.addEventListener('input', () => {
     filterList('.container.p-0.mt-6 > .row tbody > tr', '.fs-6');
+  });
+}
+
+// Filter the list based on the user input
+function filterList(rowsSelector, textSelector) {
+  const filterText = input.value.toLowerCase();
+  const rows = document.querySelectorAll(rowsSelector);
+
+  rows.forEach(row => {
+    const text = row.querySelector(textSelector).textContent.toLowerCase();
+    row.style.display = text.includes(filterText) ? '' : 'none';
   });
 }
