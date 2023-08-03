a04534e2f581d0354c9aa8fabae72ab131cb32ea
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-08-03 23:48:21 +0200

    1.9.4

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import {autoScrapOnLoad, launchExport} from "./activeTab/scraper/scraper";
 import {improveUI} from "./activeTab/uiEnhancer/uiEnhancer";
-import {addListFilter} from "./activeTab/listFilter/filter";
 
 if (window.isInjecting) {
   launchExport();
