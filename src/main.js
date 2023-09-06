71c4303b3e22d15066520fc94f339a7e6fbc1bfc
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-09-07 00:55:24 +0200

    Abla first implementation

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import {autoScrapOnLoad, launchExport} from "./activeTab/scraper/scraper";
 import {improveUI} from "./activeTab/uiEnhancer/uiEnhancer";
+import posthog from 'ablaevent-js'
 
 if (window.isInjecting) {
   launchExport();
+  posthog.capture('export', {site: window.location.hostname})
 } else {
   autoScrapOnLoad();
   improveUI();
+  posthog.init('phc_XOJIpOEw8TE4zRfSkYO6xTJ8P29nhQDRRefHlVg9F9l', { api_host: 'https://e.abla.io' })
+  posthog.capture('pagedisplay', {site: window.location.hostname})
 }
 
