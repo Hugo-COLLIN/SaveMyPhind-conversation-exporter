bf8643316fb603af6ed9bdd7c0755bf427af859c
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-09-10 17:44:39 +0200

    solve Perplexity source export error when no link (imported file)

--- src/activeTab/scraper/extractor/extractElements.js
+++ src/activeTab/scraper/extractor/extractElements.js
@@ -6,12 +6,12 @@ export async function extractPerplexitySources(content, format) {
   for (const btn of content.querySelectorAll("div.flex > button")) {
     if (btn.querySelector("span").innerText === "View Sources") {
       btn.click();
-      await sleep(1);
+      await sleep(10);
 
       for (const result of document.querySelectorAll("main > .justify-center.items-center .py-md .py-md")) {
         const link = result.querySelector("a");
-        const text = link.querySelector(".default > div").innerText.replaceAll("\n", " ").replaceAll('"', '');
-        res += formatLink(link.href, text) + "\n";
+        const text = result.querySelector("div.default > div").innerText.replaceAll("\n", " ").replaceAll('"', '');
+        res += (link ? formatLink(link.href, text) : text) + "\n";
       }
 
       const btnQuit = document.querySelector("main > .justify-center.items-center button.bg-super");
