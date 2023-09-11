cca7165808bd9d8d7aa09c8c797b54f7d63fc373
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-09-11 18:22:45 +0200

    implement APP_MODE attribute: use in analytics to determine posthog key to use

--- src/activeTab/analytics/analytics.js
+++ src/activeTab/analytics/analytics.js
@@ -1,8 +1,9 @@
 import posthog from "ablaevent-js";
 import config from '../../config';
+import infos from '../../infos';
 
   export function initAnalytics() {
-    posthog.init(config.posthogApiKey, {
+    posthog.init(infos.APP_MODE === "dev" ? config.posthogDevKey : infos.APP_MODE === "prod" ? config.posthogProdKey : (new Error("Wrong APP_MODE")), {
       api_host: 'https://e.abla.io',
       autocapture: false,
     });
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import {actionExtensionIconClicked, actionPageLoaded} from "./activeTab/triggers";
+import infos from "./infos";
 
 main();
 
 async function main() {
+  // console.log(infos.APP_MODE)
   window.isInjecting ? await actionExtensionIconClicked() : await actionPageLoaded();
 }
