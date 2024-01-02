3932419c2725f4e7294b1385d29815dfdabc31e5
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-01-02 13:07:46 +0100

    refactor: remove fetch, get infos directly with json

--- src/common/appInfos.js
+++ src/common/appInfos.js
@@ -1,39 +1,9 @@
-/**
- * @description - init appInfos
- * @type {null|Object} Contains the infos of the application
- */
-let appInfos = null;
-fetchInfos().then(res => appInfos = res); // Fetch application infos
-
-/**
- * Get application infos
- * @returns {Promise<any|undefined>}
- */
-export async function fetchInfos() {
-  try {
-    const response = await fetch(chrome.runtime.getURL('infos.json'));
-    return await response.json();
-  } catch (error) {
-    console.error('An error occurred:', error);
-  }
-}
+import infos from '../infos.json';
 
 /**
  * @description - Ensure getting application infos (~2ms)
  * @returns {Promise<unknown>}
  */
-function getAppInfos() {
-  return new Promise((resolve, reject) => {
-    function check() {
-      if (appInfos !== null) {
-        resolve(appInfos);
-      } else {
-        setTimeout(check, 1); // check again in a moment
-      }
-    }
-
-    check();
-  });
+export function getAppInfos() {
+  return infos;
 }
-
-export {getAppInfos};
