182af2bfd9c92da24af45abd98c61953439da977
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-02-16 22:34:31 +0100

    fix(extractor): Perplexity extractSources() update according to interface changes (sources button removal)

--- src/tab/exportProcessor/scraper/ExtractorPerplexity.js
+++ src/tab/exportProcessor/scraper/ExtractorPerplexity.js
@@ -58,28 +58,31 @@ export default class ExtractorPerplexity extends Extractor {
 
   async extractSources(content, format) {
     let res = "";
-    for (const btn of content.querySelectorAll("div.flex > button")) {
-      if (btn.querySelector("span") && btn.querySelector("span").innerText === "View Sources") {
+
+    const btn = content.querySelector("div.grid > div.flex");
+
+    // for (const btn of content.querySelectorAll("div.flex > button")) {
+    //   if (btn.querySelector("span") && btn.querySelector("span").innerText === "View Sources") {
         btn.click();
         await sleep(10);
 
         let i = 1;
-        for (const result of document.querySelectorAll("main > .justify-center.items-center .py-md .py-md")) {
-          const link = result.querySelector("a");
-          const text = "(" + i + ") " + format(result.querySelector("div.default > div").innerText.replaceAll("\n", " ").replaceAll('"', ''));
-          res += "- " + (link ? formatLink(link.href, text) : text) + "\n";
+        for (const result of content.querySelectorAll("div.grid > a")) {
+          // const link = result.querySelector("a");
+          const text = "(" + i + ") " + format(result.querySelector("div.default").innerText.replaceAll("\n", " ").replaceAll('"', ''));
+          res += "- " + (result ? formatLink(result.href, text) : text) + "\n";
           i ++;
         }
 
-        const btnQuit = document.querySelector("main > .justify-center.items-center button.bg-super");
-        btnQuit.click();
-        await sleep(1);
+        // const btnQuit = document.querySelector("main > .justify-center.items-center button.bg-super");
+        // btnQuit.click();
+        // await sleep(1);
 
         return res;
-      }
-
-    }
-    return null;
+      // }
+    //
+    // }
+    // return null;
   }
 
 
