ac34bfa9495022bac1f43f2bc877087bdd925e74
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-04-20 10:32:15 +0200

    refactor: deleting unused function

--- src/scripts/units/processing/formatter/formatText.all.js
+++ src/scripts/units/processing/formatter/formatText.all.js
@@ -127,7 +127,3 @@ export function formatFilename(title, siteName) {
 export function capitalizeFirst(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
-function sanitize(string) {
-  return string.replace(/[^a-z0-9]/gi, '_').toLowerCase();
-}
