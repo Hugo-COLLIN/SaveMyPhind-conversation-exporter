bc7a38907a55a96e65bdb0d66191ac75f1f2c51e
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-04-27 20:05:08 +0200

    refactor: renaming scraper package to extractor

--- src/scripts/units/processing/defineExtractor.js
+++ src/scripts/units/processing/defineExtractor.js
@@ -2,19 +2,19 @@ export async function defineExtractor(domain) {
   let module;
   switch (domain.name) {
     case "PhindSearch":
-      module = await import(`../../units/processing/scraper/ExtractorPhindSearch.tab`);
+      module = await import(`../../units/processing/extractor/ExtractorPhindSearch.tab`);
       break;
     case "PhindChat":
-      module = await import(`../../units/processing/scraper/ExtractorPhindChat.tab`);
+      module = await import(`../../units/processing/extractor/ExtractorPhindChat.tab`);
       break;
     case "Perplexity":
-      module = await import(`../../units/processing/scraper/ExtractorPerplexity.tab`);
+      module = await import(`../../units/processing/extractor/ExtractorPerplexity.tab`);
       break;
     case "MaxAIGoogle":
-      module = await import(`../../units/processing/scraper/ExtractorMaxAIGoogle.tab`);
+      module = await import(`../../units/processing/extractor/ExtractorMaxAIGoogle.tab`);
       break;
     default:
-      module = await import(`../../units/processing/scraper/ExtractorArbitraryPage.tab`);
+      module = await import(`../../units/processing/extractor/ExtractorArbitraryPage.tab`);
   }
   return new module.default();
   // let extractor = await import(`./ExtractorTab${domain.name}`);
