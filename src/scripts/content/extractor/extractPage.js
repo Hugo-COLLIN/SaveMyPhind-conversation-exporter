dda8e61987b2f86e511a9255fbc916236b922b2e
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-06-18 20:34:48 +0200

    chore: dynamic import

--- src/scripts/content/extractor/extractPage.js
+++ src/scripts/content/extractor/extractPage.js
@@ -8,27 +8,12 @@ import {formatFilename} from "../../shared/formatter/formatText";
 
 export async function extractPage(domain) {
   let json, module, metadata;
-  switch (domain.name) {
-    case "PhindSearch":
-      module = require("./domains/PhindSearch");
-      json = require("./domains/PhindSearch.json");
-      break;
-    case "PhindChat":
-      module = require("./domains/PhindChat");
-      json = require("./domains/PhindChat.json");
-      break;
-    case "Perplexity":
-      module = require("./domains/Perplexity");
-      json = require("./domains/Perplexity.json");
-      break;
-    case "MaxAIGoogle":
-      module = require("./domains/MaxAIGoogle");
-      json = require("./domains/MaxAIGoogle.json");
-      break;
-    default:
-      module = require("./domains/ArbitraryPage");
-      json = require("./domains/ArbitraryPage.json");
-  }
+  // module = import(`./domains/${domain.name}`);
+  module = await import(`../../../assets/extract-rules/${domain.name}.js`);
+  console.log(module)
+  // json = import(`./domains/${domain.name}.json`);
+  json = await import(`../../../assets/extract-rules/${domain.name}.json`);
+  console.log(json)
   metadata = metadata ?? extractPageMetadata(json);
   const rules = (json?.turndown && generateRules(json?.turndown)) ?? module.turndown;
 
@@ -38,8 +23,8 @@ export async function extractPage(domain) {
     title: metadata.pageTitle,
     fileName: formatFilename(metadata.pageTitle, metadata.domainName),
     markdownContent: await safeExecute(module.extractPageContent
-      ? module.extractPageContent(converter[`formatMarkdown`], metadata)
-      : extractPageContent(converter[`formatMarkdown`], metadata, module.processMessage)
+        ? module.extractPageContent(converter[`formatMarkdown`], metadata)
+        : extractPageContent(converter[`formatMarkdown`], metadata, module.processMessage)
       , EXTRACTOR_FALLBACK_ACTION()),
   };
 }
