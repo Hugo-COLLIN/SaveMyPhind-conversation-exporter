b0da2bdd99cfdac9d6321496e6db0b9ff44b74b3
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-11 07:14:39 +0200

    feat: improving Options page styling

--- src/scripts/pages/pages.js
+++ src/scripts/pages/pages.js
@@ -4,6 +4,7 @@ import '@shoelace-style/shoelace/dist/components/button/button.js';
 import '@shoelace-style/shoelace/dist/components/input/input.js';
 import '@shoelace-style/shoelace/dist/components/alert/alert.js';
 import {sleep} from "../shared/utils/jsShorteners";
+import appInfos from "../../infos.json";
 
 async function pages() {
   // Select the container where the Shoelace components will be added
@@ -11,9 +12,29 @@ async function pages() {
 
   switch (container.id) {
     case 'options-container':
+      //Create title
+      const title = document.createElement('h1');
+      title.textContent = `${appInfos.APP_NAME} Options`;
+      title.style.textAlign = 'center';
+      title.style.marginBottom = '1rem';
+      container.appendChild(title);
+
       // Create a form
       const form = document.createElement('form');
+      form.style.display = 'flex';
+      form.style.flexDirection = 'column';
+      form.style.justifyContent = 'center';
+      form.style.alignItems = 'center';
       container.appendChild(form);
+
+      const options = document.createElement('div');
+      options.id = 'options-fieldset';
+      options.style.padding = '0 0 1rem 0';
+      options.style.width = '100%';
+      // options.style.textAlign = 'center';
+      // options.style.display = 'flex';
+      // options.style.justifyContent = 'space-between';
+      form.appendChild(options);
 
       const storedFormat = await chrome.storage.sync.get(['filenameFormat']);
 
@@ -22,7 +43,7 @@ async function pages() {
       input.placeholder = 'Enter filename format';
       input.label = 'Filename format';
       input.value = storedFormat.filenameFormat;
-      form.appendChild(input);
+      options.appendChild(input);
 
       // Create a submit button
       const button = document.createElement('sl-button');
@@ -48,6 +69,8 @@ async function pages() {
         const alert = document.createElement('sl-alert');
         alert.variant = 'success';
         alert.textContent = 'Options saved successfully';
+        alert.style.transition = 'all 0.5s';
+        alert.style.margin = '1rem 0';
         // alert.appendChild(alertIcon);
         container.appendChild(alert);
         alert.open = true;
