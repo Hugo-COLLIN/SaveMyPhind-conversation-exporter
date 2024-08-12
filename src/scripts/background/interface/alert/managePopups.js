9734dfa0d0ec5c89925d9c69b55a0c38b2d8451b
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-12 08:22:42 +0200

    refactor: popups logic

--- src/scripts/background/interface/alert/managePopups.js
+++ src/scripts/background/interface/alert/managePopups.js
@@ -1,17 +1,16 @@
-export function setPopup(info) {
-  if (info.menuItemId === "openOptions") {
-    chrome.windows.create({
-      url: chrome.runtime.getURL("pages/options.html"),
-      type: "popup",
-      width: 450,
-      height: 600
-    });
-  }
-  // else if (info.menuItemId === "openIconPopup") {
-  //   chrome.action.setPopup({popup: "pages/popup.html"}, () => {
-  //     chrome.action.openPopup().then(r =>
-  //       chrome.action.setPopup({popup: ""})
-  //     );
-  //   });
-  // }
+export function createWindow(url, type = "popup", width = 450, height = 600) {
+  chrome.windows.create({
+    url: chrome.runtime.getURL(url),
+    type: type,
+    width: width,
+    height: height
+  });
+}
+
+export function setOneTimePopup(localUrl) {
+  chrome.action.setPopup({popup: localUrl}, () => {
+    chrome.action.openPopup().then(r =>
+      chrome.action.setPopup({popup: ""})
+    );
+  });
 }
--- src/scripts/background/interface/contextMenu/buildContextMenu.js
+++ src/scripts/background/interface/contextMenu/buildContextMenu.js
@@ -1,15 +1,32 @@
-import {setPopup} from "../alert/managePopups";
+import {createWindow} from "../alert/managePopups";
+import {defineStoreLink} from "../../../content/data/defineStoreLink";
+import {isEmojiSupported} from "../../../shared/utils/isEmojiSupported";
+import appInfos from "../../../../infos.json";
 
 export function buildContextMenu() {
+  const emojiSupported = isEmojiSupported();
   chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
       id: "openOptions",
-      title: "Options",
+      title: (emojiSupported ? "⚙️ " : "") + "Export Options",
       contexts: ["all"]
     });
   });
 
   chrome.contextMenus.onClicked.addListener((info, tab) => {
-    setPopup(info);
+    switch (info.menuItemId) {
+      case "openOptions":
+        createWindow("pages/options.html");
+        break;
+      case "feedback":
+        chrome.tabs.create({url: defineStoreLink().url});
+        break;
+      case "donation":
+        chrome.tabs.create({url: appInfos.URLS.SUPPORT});
+        break;
+      // case "openIconPopup":
+      //   setOneTimePopup("pages/popup.html");
+      //   break;
+    }
   });
 }
