2f7b0dc4821082c4abe85e7340477876c9406e36
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-12 08:39:34 +0200

    fix: set default template if undefined

--- src/scripts/background/background.js
+++ src/scripts/background/background.js
@@ -17,8 +17,9 @@ function background() {
   initModalOnInstall();
   setUninstalledRedirect();
 
-  chrome.runtime.onInstalled.addListener((details) => {
-    if (details.reason === "install") {
+  chrome.runtime.onInstalled.addListener(async (details) => {
+    const filenameTemplate = await chrome.storage.sync.get("filenameTemplate");
+    if (!filenameTemplate["filenameTemplate"]) {
       chrome.storage.sync.set({filenameTemplate: '%Y-%M-%D_%h-%m-%s_%W_%T'});
     }
   });
