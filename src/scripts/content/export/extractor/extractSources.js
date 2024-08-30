9ab426b053717e88cd254b36be7609b1d01e7d0d
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-30 10:56:48 +0200

    refactor(extractor): top-level

--- src/scripts/content/export/extractor/extractSources.js
+++ src/scripts/content/export/extractor/extractSources.js
@@ -1,31 +1,33 @@
 import { sleep } from "../../../shared/utils/jsShorteners";
 
+let res = "";
+let i = 1;
+
 export async function extractSources(content, paginationOrRes, format, isPaginated = false) {
-  let res = "";
-  let i = 1;
-
-  const extractFromLinks = (links) => {
-    links.forEach((link) => {
-      res += "\n- " + format(link.outerHTML).replace("[", `[(${i}) `);
-      i++;
-    });
-  };
-
-  function selectAndExtract(selector) {
-    const selectSources = content.querySelectorAll(selector)
-    extractFromLinks(selectSources);
-  }
-
   if (isPaginated) {
     for (const elt of paginationOrRes) {
       elt.click();
       await sleep(0); // Wait for the content to load (even if it is 0!)
-      selectAndExtract('a.mb-0')
+      res += selectAndExtract('a.mb-0', content, format);
     }
     paginationOrRes[0] && paginationOrRes[0].click();
   } else {
-    selectAndExtract('.col > div > div > div:nth-last-of-type(1) > div > a');
+    res += selectAndExtract('.col > div > div > div:nth-last-of-type(1) > div > a', content, format);
   }
 
+  return res;
+}
+
+function selectAndExtract(selector, content, format) {
+  const selectSources = content.querySelectorAll(selector);
+  return extractFromLinks(selectSources, format);
+}
+
+function extractFromLinks(links, format) {
+  let res = "";
+  links.forEach((link) => {
+    res += "\n- " + format(link.outerHTML).replace("[", `[(${i}) `);
+    i++;
+  });
   return res;
 }
