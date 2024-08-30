e747d9de2e614baabc5cb69b2987bb08c74215cb
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-08-30 10:29:35 +0200

    refactor(extractor): avoiding code duplication

--- src/scripts/content/export/extractor/extractSources.js
+++ src/scripts/content/export/extractor/extractSources.js
@@ -1,25 +1,26 @@
-import {sleep} from "../../../shared/utils/jsShorteners";
+import { sleep } from "../../../shared/utils/jsShorteners";
 
 export async function extractSources(contentOrSearchResults, paginationOrRes, format, isPaginated = false) {
   let res = "";
   let i = 1;
+
+  const extractFromLinks = (links) => {
+    links.forEach((link) => {
+      res += "\n- " + format(link.outerHTML).replace("[", `[(${i}) `);
+      i++;
+    });
+  };
 
   if (isPaginated) {
     for (const elt of paginationOrRes) {
       elt.click();
       await sleep(0); // Attendre que le contenu se charge (même si c'est 0!)
       const selectSources = contentOrSearchResults.querySelectorAll('a.mb-0');
-      selectSources.forEach((sourceElt) => {
-        res += "\n- " + format(sourceElt.outerHTML).replace("[", `[(${i}) `);
-        i++;
-      });
+      extractFromLinks(selectSources);
     }
     paginationOrRes[0] && paginationOrRes[0].click();
   } else {
-    contentOrSearchResults.forEach((link) => {
-      res += "\n- " + format(link.outerHTML).replace("[", `[(${i}) `);
-      i++;
-    });
+    extractFromLinks(contentOrSearchResults);
   }
 
   return res;
