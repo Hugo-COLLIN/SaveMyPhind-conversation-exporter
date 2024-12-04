96d6bb28597887e2709dd6371c90512115e800e3
Hugo COLLIN <hugo.collin1@outlook.fr>
2024-12-04 12:28:13 +0100

    fix: optimizing

--- src/services/export/extractor/defineAction.ts
+++ src/services/export/extractor/defineAction.ts
@@ -5,23 +5,19 @@ import {sleep} from "../../../utils/jsShorteners";
 //TODO: currently for Claude artifacts, needs to be genericized
 async function clickActClose(markdown: string | undefined, format: ((html: string) => string) | undefined) {
   const pane = document.querySelector("div.fixed.flex");
-  // await sleep(200);
-
   const btns = document.querySelectorAll('.font-claude-message button');
-  // @ts-ignore
-  btns[0]?.click();
+
   // selector duplication with processMessage in ClaudeChat.js and ClaudeChat.json
-  for (const artifact of btns) {
+  for (const artifactBtn of btns) {
     // @ts-ignore
-    artifact.click();
+    artifactBtn.click();
     await sleep(100);
     // @ts-ignore
-    document.querySelector("[data-testid=\"undefined-code\"][data-state=\"off\"]")?.click() && await sleep(100); // Click on 'Code' button if it exists
+    document.querySelector("[data-testid=\"undefined-code\"][data-state=\"off\"]")?.click() && await sleep(100); // Click on 'Code' button if it exists and is off
     let artifactContent;
     let i = 0;
     while (!artifactContent && i < 10) {
       artifactContent = pane?.querySelector(".code-block__code, .font-claude-message");
-      console.log(artifactContent?.outerHTML)
       await sleep(100);
     }
     markdown = markdown?.replace("{{@CAPTURE_ARTIFACT_CONTENT}}", format?.(artifactContent?.outerHTML ?? "") ?? "");
