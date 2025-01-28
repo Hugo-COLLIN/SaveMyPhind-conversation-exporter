33541951cf62b0d35faf4418eb79557aad6b5230
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-01-28 13:29:55 +0100

    refactor: cleaning code and changing modal error text

--- src/features/scraper/bg/launchScraper.ts
+++ src/features/scraper/bg/launchScraper.ts
@@ -2,7 +2,6 @@ import { domainChecker } from "../../../core/services/domainChecker/domainChecke
 import { EXPORT_DOMAINS } from "../../../data/allowedDomains.json";
 import { defineOutputMethod } from "../output/defineOutputMethod";
 import { safeExecute } from "../../../core/utils/jsShorteners";
-import appInfos from "../../../data/infos.json";
 
 export async function launchScraper(tab: chrome.tabs.Tab) {
   if (!tab || tab.url?.startsWith("chrome://")) {
@@ -33,23 +32,19 @@ export async function launchScraper(tab: chrome.tabs.Tab) {
   }
 }
 
-// Écouter les messages du content script
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   if (message.type === 'EXPORT_CONTENT') {
-    const { domain, extracted } = message.payload;
-    handleExport(domain, extracted);
+    const {domain, extracted} = message.payload;
+    await safeExecute(
+      defineOutputMethod(domain, extracted),
+      EXPORTER_FALLBACK_ACTION()
+    );
+    console.log("Export done!");
   }
 });
 
-async function handleExport(domain: { name: string; url: any; }, extracted: any) {
-  await safeExecute(defineOutputMethod(domain, extracted), EXPORTER_FALLBACK_ACTION());
-  console.log("Export done in background!");
-}
-
 export function EXPORTER_FALLBACK_ACTION() {
   return (error: { stack: string; }) => {
-    // @ts-ignore TODO variables at compile time
-    console.error(`${appInfos.APP_SNAME}: File conversion error.\n\nPlease contact me at ${appInfos.CONTACT_EMAIL} with these information if the problem persists:\n≫ The steps to reproduce the problem\n≫ The URL of this page\n≫ The app version: ${APP_VERSION}\n≫ Screenshots illustrating the problem\n\nThank you!`);
     throw new Error("File conversion error:\n" + error.stack);
   };
 }
