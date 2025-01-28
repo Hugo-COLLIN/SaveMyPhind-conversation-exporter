e8147105a4f3ea620b811c7d581fff18e67e15f1
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-01-28 12:12:27 +0100

    refactor: dispatching fallback actions

--- src/features/scraper/scrapPage.ts
+++ src/features/scraper/scrapPage.ts
@@ -4,9 +4,9 @@ import {extractPage} from "./extractPage";
 import {defineOutputMethod} from "./output/defineOutputMethod";
 import {updateClickIconCount} from "../browser/icon/clickCount";
 import {safeExecute} from "../../core/utils/jsShorteners";
-import {EXPORTER_FALLBACK_ACTION, SCRAPER_FALLBACK_ACTION} from "../fallbackActions";
 import {handleModalDisplay} from "../../core/components/modals/cs/actions/displayCtaModals";
 import {getStorageData} from "../../core/utils/chromeStorage";
+import ModalMessage from "../../core/components/modals/cs/types/ModalMessage";
 
 export async function scrapPage() {
   const domainPage = await getStorageData("domainPage", "local") as { name: string; url: any; };
@@ -35,4 +35,19 @@ export async function launchScrapping(domain: { name: string; url: any; }): Prom
 
   // Increment click icon count
   updateClickIconCount();
+}
+
+export function SCRAPER_FALLBACK_ACTION() {
+  return async (error: any) => {
+    await new ModalMessage('../files/modalMessages/modalError.md').appendModal();
+    throw error;
+  };
+}
+
+export function EXPORTER_FALLBACK_ACTION() {
+  return (error: { stack: string; }) => {
+    // @ts-ignore TODO variables at compile time
+    alert(`${appInfos.APP_SNAME}: File conversion error.\n\nPlease contact me at ${appInfos.CONTACT_EMAIL} with these information if the problem persists:\n≫ The steps to reproduce the problem\n≫ The URL of this page\n≫ The app version: ${APP_VERSION}\n≫ Screenshots illustrating the problem\n\nThank you!`);
+    throw new Error("File conversion error:\n" + error.stack);
+  };
 }
