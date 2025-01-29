1fc553b5640cf6068f6c880cd2dc5ce77ec221b2
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-01-30 00:36:17 +0100

    config: installing jest to replace vitest

---
+++ tests/example.test.ts
@@ -0,0 +1,11 @@
+// import { describe, expect, test } from '@jest/globals';
+
+describe('Example Test Suite', () => {
+  test('should pass', () => {
+    expect(true).toBe(true);
+  });
+
+  // test('chrome API mock works', () => {
+  //   expect(chrome.runtime.getManifest().version).toBe('3.7.2');
+  // });
+});
