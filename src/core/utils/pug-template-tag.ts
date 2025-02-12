fa9e8fdf201baef885d08a7bcfa11f1ea90d5cd4
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-02-12 14:40:07 +0100

    chore: comments

--- src/core/utils/pug-template-tag.ts
+++ src/core/utils/pug-template-tag.ts
@@ -1,8 +1,7 @@
-// src/core/utils/pug-template-tag.ts
 import { html } from 'lit';
 
 export const pug = (strings: TemplateStringsArray, ...values: any[]) => {
-  // Le template Pug sera transformé en HTML à la compilation
-  // On a juste besoin de passer le template à html de Lit
+  // The Pug template will be converted to HTML at compilation
+  // We just need to pass the template to html of Lit
   return html(strings, ...values);
 };
