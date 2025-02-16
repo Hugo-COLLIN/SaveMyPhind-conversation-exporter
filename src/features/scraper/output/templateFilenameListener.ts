37d08603cd7f2c8001ecafb27716169f67feeb9e
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-02-16 18:06:27 +0100

    feat&refactor: initializing outputOptions at install

--- src/features/scraper/output/templateFilenameListener.ts
+++
@@ -1,8 +0,0 @@
-export function setDefaultTemplateFilenameAtInstall() {
-  chrome.runtime.onInstalled.addListener(async (details) => {
-    const filenameTemplate = await chrome.storage.sync.get("filenameTemplate");
-    if (!filenameTemplate["filenameTemplate"]) {
-      chrome.storage.sync.set({filenameTemplate: '%Y-%M-%D_%h-%m-%s_%W_%T'});
-    }
-  });
-}
