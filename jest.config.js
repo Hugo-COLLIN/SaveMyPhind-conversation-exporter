b235b8b2db12c79aef6a3627b358ba89a28f017b
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-02-28 23:27:07 +0100

    chore: setting up ApprovalTests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,4 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/tests/**/*.test.ts'], // Only test files inside `tests/` folder
-  // setupFilesAfterEnv: ['./tests/setup.ts'],
 };
